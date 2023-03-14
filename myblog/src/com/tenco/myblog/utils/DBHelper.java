5094845ee20f8b7a0901fd9216c29553b2b2ad28
afeelingeffort <kmg1151@naver.com>
2023-03-14 09:42:51 +0900

    초기 패키지 구성

---
+++ myblog/src/com/tenco/myblog/utils/DBHelper.java
@@ -0,0 +1,16 @@
+package com.tenco.myblog.utils;
+
+/**
+ * 
+ * @author 
+ * mysql 서버와 연결을 해주는 객체
+ */
+public class DBHelper {
+
+	private static final String DB_HOST = "localhost";
+	private static final String DB_PORT = "3306";
+	private static final String DB_DATABASE_NAME = "myblog";
+	private static final String DB_CHARSET = "UTF-8";
+	private static final String DB_USER_NAME = "bloguser";
+	private static final String DB_PASSWORD = "1q2w3e4r5t";
+}
