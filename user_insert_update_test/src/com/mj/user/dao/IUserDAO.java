63a721a7c92f749c1e75b4ddf5d44044e8b10d20
afeelingeffort <105858187+afeelingeffort@users.noreply.github.com>
2023-03-27 16:06:28 +0900

    Delete user_insert_update_test directory

--- user_insert_update_test/src/com/mj/user/dao/IUserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.mj.user.dao;
-
-import com.mj.user.dto.UserDTO;
-
-// insert, update
-public interface IUserDAO {
-	int insert(UserDTO dto);
-	int update(int id, int targetId, int tagetPassword);
-}
